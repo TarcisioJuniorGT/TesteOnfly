b210d952a468e2863a7151edc7f989d96a2a2f15
Tarcisio Pais de Souza Junior <tarcisio.souza@rehagro.edu.br>
2023-04-02 23:52:32 -0300

    New test:testStore.

--- tests/Feature/Expense/ExpenseTest.php
+++ tests/Feature/Expense/ExpenseTest.php
@@ -30,4 +30,29 @@ class ExpenseTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testStore()
+    {
+        $user = User::factory(User::class)->create();
+        $token = JWTAuth::fromUser($user);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/expenses', [
+            'id_user' => $user->id,
+            'value' => 19.50,
+            'date' => '2023-04-02',
+            'email' => $user->email,
+            'description' => 'This is an example resource.',
+        ]);
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('expenses', [
+            'id_user' => $user->id,
+            'value' => 19.50,
+            'date' => '2023-04-02',
+            'email' => $user->email,
+            'description' => 'This is an example resource.',
+        ]);
+    }
 }
